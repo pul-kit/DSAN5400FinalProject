9158f9e6af9af9914c301338a81fe12e460303fe
elizarice <143218004+elizarice@users.noreply.github.com>
2023-11-28 21:15:42 -0500

    Update main.py (with code to evaluate test data)

--- final_project/chatbot/chatbot/bin/main.py
+++ final_project/chatbot/chatbot/bin/main.py
@@ -1,5 +1,13 @@
+import sys
+import os
 
-from chatbot.search_engine.tf_idf_searcher import SearchEngine
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(parent_dir)
+
+from search_engine.tf_idf_searcher import SearchEngine
+
+from eval.eval import get_test_ids, get_seach_engine_ids, evaluate
 
 
 
@@ -10,3 +18,17 @@ if __name__ == '__main__':
 
 
     print(searcher.search(query))
+
+    # code to evaluate search engine on test data in test_data.csv
+    test_ids = get_test_ids()
+    search_engine_ids = get_seach_engine_ids() 
+
+    prec, recall, f1, MAP = evaluate(test_ids, search_engine_ids) 
+    print(f'\nEvaluation metrics:')
+    print(f'Precision: {prec}')
+    print(f'Recall: {recall}')
+    print(f'F-1 score: {f1}')
+    print(f'Mean Average Precision (MAP) score: {MAP}\n')
+
+    print(f'True document ids for test queries: {test_ids}')
+    print(f'Search engine predictions for doc_ids of test queries: {search_engine_ids}\n')
