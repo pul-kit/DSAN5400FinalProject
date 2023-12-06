85420998995c9972b96458a7894864591f4fcc54
elizarice <143218004+elizarice@users.noreply.github.com>
2023-12-06 13:04:17 -0500

    Add doc strings to openai_API.py

--- final_project/chatbot/chatbot/utils/openai_API.py
+++ final_project/chatbot/chatbot/utils/openai_API.py
@@ -1,6 +1,12 @@
 import openai
 
 def openai_chat(api_key, messages):
+    """ 
+    Initiates a chat conversation using the OpenAI GPT-3.5 Turbo model.
+    :param api_key: (str) Your OpenAI API key for authentication.
+    :param messages : (list) A list of message objects representing the conversation.
+    :return : (str) OpenAI model's generated repsonse. 
+    """
     openai.api_key = api_key
 
     try:
@@ -14,6 +20,11 @@ def openai_chat(api_key, messages):
 
 
 def generate(query, context):
+    """ 
+    Generates a response to a user query based on a given context using the OpenAI GPT-3.5 Turbo model.
+    :param query : (str) The user's question or query.
+    :param context : (str) Context to base the response on.
+    """
     api_key = "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"  
     messages = [
         {"role": "system", "content": "You are a helpful chatbot."},
