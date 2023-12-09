f5233f6b73893eb849b8bd01edc82193afbb5361
pul-kit <143104743+pul-kit@users.noreply.github.com>
2023-12-08 21:22:58 -0500

    Update tests.py

--- final_project/chatbot/tests/tests.py
+++ final_project/chatbot/tests/tests.py
@@ -1,10 +1,3 @@
-
-import os
-import sys
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
-sys.path.append(parent_dir)
-
 from chatbot.search_engine.tf_idf_searcher import SearchEngine
 searcher = SearchEngine()
 
